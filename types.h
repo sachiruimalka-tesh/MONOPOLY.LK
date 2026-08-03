1df0b0af45224db345f3e775481d0b8712cc3697
sachiruimalka-tesh <sachiruimalka@gmail.com>
2026-08-03 20:08:14 +0530

    create types.h file

---
+++ types.h
@@ -0,0 +1,184 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/*==========================
+    CONSTANTS
+==========================*/
+
+#define MAX_PLAYERS        4
+#define BOARD_SIZE         40
+#define MAX_NAME_LENGTH    50
+#define MAX_ROUNDS         500
+#define START_MONEY        30000
+
+#define GO_MONEY           2000
+#define JAIL_BAIL          300
+
+#define MAX_HOUSES         4
+#define MAX_HOTELS         1
+
+/*==========================
+    ENUMERATIONS
+==========================*/
+
+/* Board Square Types */
+typedef enum
+{
+    GO,
+    PROPERTY,
+    RAILWAY,
+    UTILITY,
+    TAX,
+    EVENT,
+    BANK,
+    INSURANCE,
+    JAIL,
+    FREE_PARKING,
+    GO_TO_JAIL
+
+} SquareType;
+
+
+/* Property Colour Groups */
+typedef enum
+{
+    BROWN,
+    LIGHT_BLUE,
+    PINK,
+    ORANGE,
+    RED,
+    YELLOW,
+    GREEN,
+    DARK_BLUE,
+    NO_GROUP
+
+} PropertyGroup;
+
+
+/* Player Strategies */
+typedef enum
+{
+    AGGRESSIVE_INVESTOR,
+    CONSERVATIVE_BANKER,
+    RISK_TAKER,
+    OPPORTUNISTIC_TRADER
+
+} PlayerType;
+
+
+/* Insurance Types */
+typedef enum
+{
+    NO_INSURANCE,
+    BASIC_INSURANCE,
+    COMPREHENSIVE_INSURANCE,
+    BUSINESS_INTERRUPTION
+
+} InsuranceType;
+
+
+/*==========================
+        LOAN
+==========================*/
+
+typedef struct
+{
+    int active;
+    int amount;
+    int interestRate;
+    int remainingRounds;
+
+} Loan;
+
+
+/*==========================
+      PROPERTY
+==========================*/
+
+typedef struct
+{
+    char name[MAX_NAME_LENGTH];
+
+    PropertyGroup group;
+
+    int purchasePrice;
+    int mortgageValue;
+    int baseRent;
+
+    int houseCost;
+    int hotelCost;
+
+    int owner;
+
+    int houses;
+    int hotel;
+
+    int mortgaged;
+
+    InsuranceType insurance;
+
+    int age;
+    int depreciation;
+
+    int condition;
+
+} Property;
+
+
+/*==========================
+      BOARD SQUARE
+==========================*/
+
+typedef struct
+{
+    int index;
+
+    SquareType type;
+
+    char name[MAX_NAME_LENGTH];
+
+    Property property;
+
+} Square;
+
+
+/*==========================
+        PLAYER
+==========================*/
+
+typedef struct
+{
+    char name[MAX_NAME_LENGTH];
+
+    PlayerType strategy;
+
+    int position;
+
+    int cash;
+
+    int inJail;
+
+    int jailTurns;
+
+    int bankrupt;
+
+    int propertiesOwned;
+
+    int railwaysOwned;
+
+    int utilitiesOwned;
+
+    Loan loan;
+
+} Player;
+
+
+/*==========================
+   GLOBAL VARIABLES
+==========================*/
+
+extern Square board[BOARD_SIZE];
+
+extern Player players[MAX_PLAYERS];
+
+#endif
