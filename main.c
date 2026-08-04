9c0695a6f4687846e9ce8a3d90818d90437c4434
sachiruimalka-tesh <sachiruimalka@gmail.com>
2026-08-04 16:02:13 +0530

    changed back to main.c

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 #include "types.h"
 
-/* Function Prototypes */
-void initializeBoard(void);
-void displayBoard(void);
-
-void initializePlayers(void);
-void displayPlayers(void);
+/* Function prototype from game.c */
+void startGame(void);
 
 int main(void)
 {
     printf("=========================================\n");
-    printf("       MONOPOLY-LK SIMULATION\n");
-    printf("=========================================\n");
+    printf("        MONOPOLY-LK SIMULATION\n");
+    printf("=========================================\n\n");
 
-    initializeBoard();
-    initializePlayers();
-
-    displayBoard();
-
-    displayPlayers();
+    startGame();
 
     return 0;
 }
